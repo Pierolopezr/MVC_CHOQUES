e526ec952a1f3a648491360066fd1a1eeadd31d0
piero <plopezrosas@danielcastelao.org>
2025-05-21 23:30:54 +0200

    View comentado

--- src/View.java
+++ src/View.java
@@ -1,53 +1,80 @@
 import java.util.Scanner;
 
 /**
- * Clase encargada de la interacción con el usuario
+ * @author Piero López
+ * Clase encargada de la interacción con el usuario.
+ * Muestra menús y pide información por consola.
  */
 public class View {
 
     /**
-     * Menu inicial
+     * Muestra el menú principal y gestiona la entrada del usuario.
      */
     public static void menu(){
-        int accion=0;
+        int accion = -1;
         Scanner sc = new Scanner(System.in);
-        while (accion !=4) {
+
+        while (accion != 0) {
+            // Opciones disponibles
             System.out.println("1, Crear coche");
-            System.out.println("2, Cambiar velocidad");
-            System.out.println("3, Mostrar velocidad");
-            System.out.println("4, Salir");
-            accion=sc.nextInt();
+            System.out.println("2, subir velocidad");
+            System.out.println("3, bajar velocidad");
+            System.out.println("4, Mostrar todos los coches");
+            System.out.println("0, Salir");
+
+            accion = sc.nextInt();
+
+            // Manejo del menú según la opción
             switch (accion){
-                case 1-> {
-                    System.out.println(Controller.crearCoche());
-                }
-                default -> accion=4;
+                case 1 -> System.out.println(Controller.crearCoche());
+                case 2 -> System.out.println(Controller.subirVelocidad());
+                case 3 -> System.out.println(Controller.bajarVelocidad());
+                case 4 -> System.out.println(Controller.mostrarCoches());
+                default -> accion = 0; // Salida
             }
         }
     }
 
     /**
-     * Muestra la velocidad de un coche
-     * @param matricula del coche
-     * @param v velocidad
-     * @return true si se ha mostrado correctamente
+     * Pide el modelo del coche al usuario.
+     * @param msg mensaje a mostrar
+     * @return modelo ingresado
      */
-    public static boolean muestraVelocidad(String matricula, Integer v){
-        System.out.println(matricula + ": " + v + "km/hr");
-        return true;
-    }
-
     public static String pedirModelo(String msg){
         Scanner sc = new Scanner(System.in);
         System.out.println(msg);
-        String modelo = sc.next();
-        return modelo;
+        return sc.next();
     }
 
+    /**
+     * Pide la matrícula del coche al usuario.
+     * @param msg mensaje a mostrar
+     * @return matrícula ingresada
+     */
     public static String pedirMatricula(String msg){
         Scanner sc = new Scanner(System.in);
         System.out.println(msg);
-        String matricula = sc.next();
-        return matricula;
+        return sc.next();
+    }
+
+    /**
+     * Pide una velocidad al usuario.
+     * @param msg mensaje a mostrar
+     * @return velocidad ingresada
+     */
+    public static int pedirVelocidad(String msg){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(msg);
+        return sc.nextInt();
+    }
+
+    /**
+     * Muestra un mensaje de error por consola.
+     * @param msg mensaje de error
+     * @return true si se mostró correctamente
+     */
+    public static boolean mensajeError(String msg){
+        System.out.println(msg);
+        return true;
     }
 }
